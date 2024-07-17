6f90fe3e347159035c4fc0bda3848d9f499a897a
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-07-17 17:57:45 -0300

    alterando caminho da api

--- main.py
+++ main.py
@@ -1,4 +1,53 @@
-import uvicorn
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True, proxy_headers=True)
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.controllers.BrapiStockController import BrapiStockController
+from app.crons.ActiveStocksCronJob import ActiveStocksCronJob
+
+from app.sql.database import get_db
+from app.sql.crud import Crud
+
+scheduler = BackgroundScheduler()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+]
+
+scheduler.add_job(ActiveStocksCronJob.get_updated_stocks, 'interval', minutes=43200)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Startup")
+    if not scheduler.running:
+        scheduler.start()
+    ActiveStocksCronJob.get_updated_stocks()
+    yield
+    print("Shutdown")
+    scheduler.shutdown()
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/stocks", tags=["stocks"])
+def get_stocks(db: Session = Depends(get_db)):
+    return Crud(db).get_all_ative_stocks()
+
+@app.get("/stocks/{ticker}", tags=["stocks"])
+def get_stock_info(ticker: str):
+    return BrapiStockController().get_stock_info(ticker)
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
