060873deab422e171dc0ce1cd682721ae24b93f5
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-07-23 23:41:56 -0300

    fazendo as migrations funcionarem no build

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
 
+echo "Cleaning database..."
+rm test.db || { echo "Failed to clean database"; exit 1; }
+echo "DB cleaned"
+
 echo "Running alembic migrations..."
 alembic upgrade head || { echo "Failed to run alembic migrations"; exit 1; }
 
