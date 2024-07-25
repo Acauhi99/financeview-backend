d9bae782b607cab1f51c81be5e8f08bbd8635214
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-07-25 17:09:50 -0300

    :rocket: refatorado user_controller

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,128 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError, OperationalError
+from app.auth.token_auth import TokenAuth
+from app.sql.models import User
+from app.sql.dtos import UserCreateDTO, UserLoginDTO, UserUpdateDTO
+from fastapi.exceptions import HTTPException
+from fastapi import status
+from app.auth.token_auth import TokenAuth
+
+class UserController:
+    def __init__(self, db: Session):
+        self.token_create = TokenAuth(db).token_create
+        self.crypt_context = TokenAuth(db).crypt_context
+        self.db = db
+    
+    def create_user(self, user: UserCreateDTO) -> dict:
+        user_model = User(
+            name=user.name,
+            email=user.email,
+            hashed_password=self.crypt_context.hash(user.password)
+        )
+        try:
+            self.db.add(user_model)
+            self.db.commit()
+        except OperationalError:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Database table not found'
+            )
+        except IntegrityError:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail='User already exists'
+            )
+        except Exception as e:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
+            )
+        
+        return {
+            "message": "User created successfully",
+            "status_code": status.HTTP_201_CREATED
+        }
+        
+    def user_login(self, user: UserLoginDTO) -> dict:
+        user_exists = self.db.query(User).filter_by(email=user.email).first()
+
+        if not user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Email or password is incorrect'
+            )
+
+        if user_exists and not user_exists.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_300_MULTIPLE_CHOICES,
+                detail='User is inactive'
+            )
+        
+        if not self.crypt_context.verify(user.password, user_exists.hashed_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Email or password is incorrect'
+            )
+        
+        return self.token_create(user_exists)
+
+    def update_user(self, user_id: int, user: UserUpdateDTO) -> dict:
+        user_exists = self.db.query(User).filter_by(id=user_id).first()
+
+        if not user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+        try:
+            user_exists.name = user.name
+            user_exists.email = user.email
+            user_exists.url_image = user.url_image
+            self.db.commit()
+        except IntegrityError:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail='User already exists'
+            )
+        except Exception as e:
+            self.db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
+            )
+        
+        return {
+            'message': 'User updated',
+            'status_code': status.HTTP_200_OK
+        }
+    
+    def desactive_user(self, user_id: int) -> dict:
+        user_exists = self.db.query(User).filter_by(id=user_id).first()
+        if not user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+        
+        user_exists.is_active = False
+        self.db.commit()
+        return {
+            'message': 'User desactive successfully',
+            'status_code': status.HTTP_204_NO_CONTENT
+        }
+    
+    def reactive_user(self, user_email: str) -> dict:
+        user_exists = self.db.query(User).filter_by(email=user_email).first()
+        if not user_exists:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+            )
+        
+        user_exists.is_active = True
+        self.db.commit()
+        return self.token_create(user_exists)
--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from app.controllers.feedback_controller import FeedbackAuth
+from app.controllers.feedback_controller import FeedbackController
+from app.controllers.user_controller import UserController
 from app.sql.database import get_db
-from app.auth.user_auth import UserAuth
 from app.auth.verifier_auth import token_verify
 from app.sql.dtos import *
 from fastapi.security import OAuth2PasswordRequestForm
@@ -10,30 +10,40 @@ from fastapi.security import OAuth2PasswordRequestForm
 router = APIRouter(prefix='/user', tags=['user'])
 
 @router.post("/register")
-def user_register(user: UserCreateDTO, db: Session = Depends(get_db)):
-    UserAuth(db).create_user(user)
-    return {
-        "message": "User created successfully",
-        "status_code": status.HTTP_201_CREATED
-    }
+def user_register(user: UserCreateDTO, 
+                  db: Session = Depends(get_db)):
+    return UserController(db).create_user(user)
 
 @router.post("/login")
-def user_login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = UserLoginDTO(
-        email=form_data.username, 
-        password=form_data.password)
-    
-    token_data = UserAuth(db).user_login(user)
+def user_login(form_data: OAuth2PasswordRequestForm = Depends(), 
+               db: Session = Depends(get_db)):
+    user = UserLoginDTO(email=form_data.username, password=form_data.password)
+    return UserController(db).user_login(user)
 
-    return token_data
+@router.put("/update/{user_id}", dependencies=[Depends(token_verify)])
+def user_update(user_id: int, 
+                user: UserUpdateDTO, 
+                db: Session = Depends(get_db)):
+    return UserController(db).update_user(user_id, user)
+
+@router.patch("/desactive/{user_id}", dependencies=[Depends(token_verify)])
+def user_desactive(user_id: int, 
+                   db: Session = Depends(get_db)):
+    return UserController(db).desactive_user(user_id)
+
+@router.patch("/reactive/{user_email}")
+def user_reactive(user_email: str, 
+                  db: Session = Depends(get_db)):
+    return UserController(db).reactive_user(user_email)
 
 @router.post("/feedback", dependencies=[Depends(token_verify)])
-def user_feedback(feedback: FeedbackCreateDTO, db: Session = Depends(get_db)):   
-    return FeedbackAuth(db).create_feedback(feedback)
+def user_feedback(feedback: FeedbackCreateDTO, 
+                  db: Session = Depends(get_db)):   
+    return FeedbackController(db).create_feedback(feedback)
 
 @router.get("/feedback")
 def get_user_feedback(db: Session = Depends(get_db)):
-    feedback_list = FeedbackAuth(db).get_feedback()
+    feedback_list = FeedbackController(db).get_feedback()
     return {
         "message": "Feedback retrieved successfully",
         "status_code": status.HTTP_200_OK,
