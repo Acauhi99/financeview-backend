c9a32a56d1ff072657abaa8d344679b13d1fbe00
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-07-25 17:10:35 -0300

    :bug: fix no regex de nome de usuario

--- app/sql/dtos.py
+++ app/sql/dtos.py
@@ -9,7 +9,7 @@ class UserCreateDTO(BaseModel):
 
     @field_validator('name')
     def validate_name(cls, value) -> str:
-        if not re.match('^(?!^[0-9@]+$)(?=.*[a-zA-Z])[a-zA-Z0-9@]+$', value):
+        if not re.match(r'^(?!.*  )(?!^[0-9@]+$)(?=.*[a-zA-Z])[a-zA-Z0-9@ ]+$', value):
             raise ValueError('Invalid name')
         return value
     
@@ -23,6 +23,23 @@ class UserLoginDTO(BaseModel):
     email: str
     password: str
 
+    @field_validator('email')
+    def validate_email(cls, value) -> str:
+        if not '@' in value:
+            raise ValueError('Invalid email')
+        return value
+    
+class UserUpdateDTO(BaseModel):
+    name: str
+    email: str
+    url_image: Optional[str]
+
+    @field_validator('name')
+    def validate_name(cls, value) -> str:
+        if not re.match(r'^(?!.*  )(?!^[0-9@]+$)(?=.*[a-zA-Z])[a-zA-Z0-9@ ]+$', value):
+            raise ValueError('Invalid name')
+        return value
+    
     @field_validator('email')
     def validate_email(cls, value) -> str:
         if not '@' in value:
