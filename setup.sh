049d60ef9945d4a0adf9c87235db5be91c2eabd1
Mateus Acauhi <acauhi.mateus@gmail.com>
2024-07-29 00:55:36 -0300

    # comentando o reset do banco

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ set -e
 pip install --upgrade pip
 pip install -r requirements.txt
 
-if [ -f "test.db" ]; then
-    rm test.db
-fi
+# if [ -f "test.db" ]; then
+#     rm test.db
+# fi
 
 alembic upgrade head 
